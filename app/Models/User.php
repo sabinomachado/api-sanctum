f56aab893a80e53e2b1fcb5469336422b86be9aa
sabinomachado <sabino_machado@hotmail.com>
2023-09-19 18:06:13 -0300

    auth básica

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+    public function getAuthTokenAttribute()
+    {
+        return $this->createToken('authToken')->plainTextToken;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function (){
+    Route::post('login', 'App\Http\Controllers\Auth\Api\LoginController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    route::post('register', 'App\Http\Controllers\Auth\Api\RegisterController@register');
+});
